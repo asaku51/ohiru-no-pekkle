6fb181022228020a2d84cfc38e31d2cd89c8d5ef
asaku51 <honsya@tatsuya.co.jp>
2026-09-15 12:08:50 +0900

    Create database.py

---
+++ backend/app/database.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/pekkle")
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
